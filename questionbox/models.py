d678f5d482507a7fa63e007b7e02974a3fb56333
Britton W <britton.wolfe1999@gmail.com>
2020-06-17 12:04:19 -0400

    added timestamp fields

--- questionbox/models.py
+++ questionbox/models.py
@@ -7,16 +7,19 @@ class Question(models.Model):
 	title = models.TextField(max_length = 255)
 	body = models.TextField(max_length = BODY_LENGTH)
 	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "questions")
+	timestamp = models.DateTimeField()
 
 class Answer(models.Model):
 	question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = "answers")
 	is_answer = models.BooleanField(default = False)
+	timestamp = models.DateTimeField()
 
 class Comment(models.Model):
 	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "comments")
 	question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = "comments")
 	answer = models.ForeignKey(to = Answer, on_delete = models.CASCADE, related_name = "comments", NULL = True)
 	body = models.TextField(max_length = BODY_LENGTH)
+	timestamp = models.DateTimeField()
 
 class Star(models.Model):
 	user = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = "stars")
