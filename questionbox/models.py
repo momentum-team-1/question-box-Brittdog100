27ae56cbe51859306decbea5fbc74bed797b2f3c
Britton W <britton.wolfe1999@gmail.com>
2020-06-18 14:13:24 -0400

    actually installed DRR and fixed some minor errors

--- questionbox/models.py
+++ questionbox/models.py
@@ -6,7 +6,7 @@ BODY_LENGTH = 32767
 class Question(models.Model):
 	title = models.TextField(max_length = 255)
 	body = models.TextField(max_length = BODY_LENGTH)
-	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "questions")
+	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "questions", null = True)
 	timestamp = models.DateTimeField()
 
 class Answer(models.Model):
@@ -15,13 +15,13 @@ class Answer(models.Model):
 	timestamp = models.DateTimeField()
 
 class Comment(models.Model):
-	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "comments")
+	user = models.ForeignKey(to = User, on_delete = models.SET_NULL, related_name = "comments", null = True)
 	question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = "comments")
-	answer = models.ForeignKey(to = Answer, on_delete = models.CASCADE, related_name = "comments", NULL = True)
+	answer = models.ForeignKey(to = Answer, on_delete = models.CASCADE, related_name = "comments", null = True)
 	body = models.TextField(max_length = BODY_LENGTH)
 	timestamp = models.DateTimeField()
 
 class Star(models.Model):
 	user = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = "stars")
 	question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = "stars")
-	answer = models.ForeignKey(to = Answer, on_delete = model.CASCADE, related_name = "stars", NULL = True)
+	answer = models.ForeignKey(to = Answer, on_delete = models.CASCADE, related_name = "stars", null = True)
